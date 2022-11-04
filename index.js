c696b86bd1c4fcd4ccee103768621938f940943d
AngelaW45 <113141296+AngelaW45@users.noreply.github.com>
2022-11-04 10:15:42 -0400

    2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log(conected);
+const DOMSelectors = {
+  butten: document.getElementById("btn"),
+  text: document.querySelector("#text"),
+  background: document.getElementById("background"),
+};
+
+function backgroundAndText(background, text) {
+  text.innerHTML = "This is now a pink background";
+}
+
+console.log(DOMSelectors);
