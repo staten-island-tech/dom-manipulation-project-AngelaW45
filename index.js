51e51e0c17f01b7825c75365d90eeabc7463b123
AngelaW45 <113141296+AngelaW45@users.noreply.github.com>
2022-11-10 10:15:40 -0500

    5

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ const DOMSelectors = {
   button: document.getElementById("btn"),
   text: document.querySelector("#text"),
   box: document.getElementById("background"),
+  input: document.querySelector(`#input`),
 };
 
-function backgroundAndText(background, text) {
+/* function backgroundAndText(background, text) {
   background.style.backgroundColor = "pink";
   text.textContent = "This is now a pink background";
-}
+} */
+
+/* DOMSelectors.button.addEventListener("click", function () {
+  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+}); */
 
 DOMSelectors.button.addEventListener("click", function () {
-  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+  let input = DOMSelectors.input.value;
+  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
+  DOMSelectors.input.value = "";
 });
