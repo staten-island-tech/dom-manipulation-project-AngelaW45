960c30ed8c06757d3a3e9484e3cf8f10d7c6b36d
AngelaW45 <113141296+AngelaW45@users.noreply.github.com>
2022-11-14 10:16:36 -0500

    6

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const DOMSelectors = {
-  button: document.getElementById("btn"),
+  submit: document.querySelector(".btn"),
   text: document.querySelector("#text"),
-  box: document.getElementById("background"),
-  input: document.querySelector(`#input`),
+  box: document.getElementById("song"),
 };
+
+DOMSelectors.submit.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log(event);
+});
 
 /* function backgroundAndText(background, text) {
   background.style.backgroundColor = "pink";
@@ -14,8 +18,16 @@ const DOMSelectors = {
   backgroundAndText(DOMSelectors.box, DOMSelectors.text);
 }); */
 
-DOMSelectors.button.addEventListener("click", function () {
+/* const DOMSelectors = {
+  button: document.getElementById("btn"),
+  text: document.querySelector("#text"),
+  box: document.getElementById("song"),
+  input: document.querySelector(`btn`),
+}; */
+
+/* DOMSelectors.button.addEventListener("click", function () {
   let input = DOMSelectors.input.value;
   DOMSelectors.box.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
   DOMSelectors.input.value = "";
 });
+ */
