a807172d6d10c8084bd337e39038fcdd1cebf3e3
AngelaW45 <113141296+AngelaW45@users.noreply.github.com>
2022-12-01 09:54:51 -0500

    15

--- index.js
+++ index.js
@@ -9,20 +9,6 @@ const DOMSelectors = {
   URL: document.getElementById("url"),
 };
 
-function clearfields() {
-  DOMSelectors.Album.value = "";
-  DOMSelectors.Artist.value = "";
-  DOMSelectors.URL.value = "";
-}
-
-DOMSelectors.submitForm.addEventListener("submit", function (event) {
-  let Album = DOMSelectors.Album.value;
-  let Artist = DOMSelectors.Artist.value;
-  let URL = DOMSelectors.URL.value;
-
-  event.preventDefault();
-});
-
 function card(Album, Artist, URL) {
   DOMSelectors.displaySection.insertAdjacentHTML(
     "beforeend",
@@ -39,35 +25,25 @@ function card(Album, Artist, URL) {
   );
 }
 
-clearfields();
+function clearfields() {
+  DOMSelectors.Album.value = "";
+  DOMSelectors.Artist.value = "";
+  DOMSelectors.URL.value = "";
+}
 
-let removebtn = document.querySelectorAll(".remove-btn");
+DOMSelectors.submitForm.addEventListener("submit", function (event) {
+  let Album = DOMSelectors.Album.value;
+  let Artist = DOMSelectors.Artist.value;
+  let URL = DOMSelectors.URL.value;
+  card(Album, Artist, URL);
+  event.preventDefault();
+  clearfields();
 
-removebtn.forEach((btn) => {
-  btn.addEventListener("click", function (event) {
-    event.target.parentElement.remove();
+  let removebtn = document.querySelectorAll(".remove-btn");
+
+  removebtn.forEach((btn) => {
+    btn.addEventListener("click", function (event) {
+      event.target.parentElement.remove();
+    });
   });
 });
-clearfields();
-
-/* function removebtn() {
-  DOMSelectors.displayCard = "";
-}
- */
-
-/* DOMSelectors.submit.addEventListener("click", function () {
-  let input = DOMSelectors.input.value;
-  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
-  DOMSelectors.input.value = "";
-}); */
-
-/* DOMSelectors.submit.addEventListener("click", function (event) {
-  event.preventDefault();
-  console.log(event);
-});
- */
-
-/* function backgroundAndText(background, text) {
-  background.style.backgroundColor = "pink";
-  text.textContent = "This is now a pink background";
-} */
